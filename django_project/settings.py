3401d9cba8679d82d4d5d73f42af43df78bf89d4
moslehazizi <moslehazizi@gmail.com>
2024-02-10 16:35:52 +0330

    add apis, cors and csrf

--- django_project/settings.py
+++ django_project/settings.py
@@ -40,11 +40,15 @@ INSTALLED_APPS = [
     # Local
     'accounts.apps.AccountsConfig',
     'posts.apps.PostsConfig',
+    # 3rd-party
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -126,3 +130,16 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "accounts.CustomUser"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
+
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:3000",
+    "http://localhost:8000",
+)
+
+CSRF_TRUSTED_ORIGIN = ["http://localhost:3000"]
