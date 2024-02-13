1c23f319650603b400fe36f136a072056e3b6aee
moslehazizi <moslehazizi@gmail.com>
2024-02-13 23:14:45 +0330

    add permission for registration

--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,7 @@ from .models import Post
 from .serializers import PostSerializers, UserSerializer
 from .permissions import IsAuthorOrReadOnly
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
 class PostViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,)
@@ -10,5 +11,6 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializers
 
 class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminUser]
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
